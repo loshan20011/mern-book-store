82831a12e243422b791975d1c18e671284eb340a
loshan20011 <loshan20011@gmail.com>
2024-02-03 09:44:10 +0530

    app-connected-mongo

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://loshan20011:AHCghyrQvmpghgpl@cluster0.pzktucp.mongodb.net/BOOKS";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+app.get('/', (req, res) => {
+    console.log("varans");
+    return res.status(234);
+});
+
+
+
+mongoose.connect(mongoDBURL)
+.then(() => {
+    console.log("App connected to mongo")
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`)
+    })
 })
+.catch((error) => {
+    console.log(error)
+});
+
