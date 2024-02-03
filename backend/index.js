47f3f1a30eebb17b23784c9980f6525838a89da8
loshan20011 <loshan20011@gmail.com>
2024-02-03 12:37:46 +0530

    cors-policy-added

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors'
 
 const app = express();
 
+//middleware for handling parsing request body
 app.use(express.json());
+
+//middleware for handling cors policy
+app.use(cors());
+
 app.get('/', (req,res) => {
-    console.log(req);
     return res.status(234).send("Welcome to Book Store")
 })
 
